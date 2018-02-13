c5d46c9dad033bad6db2adcf646c16b7f24be997
KrIsKa7a <kristiqn.ivanov@abv.bg>
2018-02-13 23:00:52 +0200

    Upload the first lab of the course - Define Classes

---
+++ 01.DefineClasses - Lab/03.TestClient/BankAccount.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class BankAccount
+{
+    private int id;
+    private decimal balance;
+
+    public int Id
+    {
+        get { return this.id; }
+        set { this.id = value; }
+    }
+
+    public decimal Balance
+    {
+        get { return this.balance; }
+        set { this.balance = value; }
+    }
+
+    public void Deposit(decimal amount)
+    {
+        this.balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        this.balance -= amount;
+    }
+
+    public override string ToString()
+    {
+        return $"Account ID{this.id}, balance {this.balance:f2}";
+    }
+}
+
---
+++ 01.DefineClasses - Lab/03.TestClient/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var accounts = new Dictionary<int, BankAccount>();
+
+        var command = Console.ReadLine();
+
+        while (command != "End")
+        {
+            var cmdArgs = command
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var cmdType = cmdArgs[0];
+            var id = int.Parse(cmdArgs[1]);
+
+            if (cmdType == "Create")
+            {
+                CreateAccount(accounts, id);
+            }
+            else if (cmdType == "Deposit")
+            {
+                DepositToAccount(accounts, cmdArgs, id);
+            }
+            else if (cmdType == "Withdraw")
+            {
+                WithdrawFromAccount(accounts, cmdArgs, id);
+            }
+            else if (cmdType == "Print")
+            {
+                PrintAccount(accounts, id);
+            }
+
+            command = Console.ReadLine();
+        }
+    }
+
+    private static void PrintAccount(Dictionary<int, BankAccount> accounts, int id)
+    {
+        if (CheckIfAccountIsExistant(accounts, id))
+        {
+            Console.WriteLine("Account does not exist");
+            return;
+        }
+        var acc = accounts[id];
+        Console.WriteLine(acc);
+    }
+
+    private static void WithdrawFromAccount(Dictionary<int, BankAccount> accounts, string[] cmdArgs, int id)
+    {
+        var amount = decimal.Parse(cmdArgs[2]);
+
+        if (CheckIfAccountIsExistant(accounts, id))
+        {
+            Console.WriteLine("Account does not exist");
+            return;
+        }
+
+        var acc = accounts[id];
+
+        if (amount > acc.Balance)
+        {
+            Console.WriteLine("Insufficient balance");
+            return;
+        }
+
+        acc.Withdraw(amount);
+        accounts[id] = acc;
+    }
+
+    private static void DepositToAccount(Dictionary<int, BankAccount> accounts, string[] cmdArgs, int id)
+    {
+        var amount = decimal.Parse(cmdArgs[2]);
+
+        if (CheckIfAccountIsExistant(accounts, id))
+        {
+            Console.WriteLine("Account does not exist");
+            return;
+        }
+
+        var acc = accounts[id];
+        acc.Deposit(amount);
+        accounts[id] = acc;
+    }
+
+    private static bool CheckIfAccountIsExistant(Dictionary<int, BankAccount> accounts, int id)
+    {
+        return !accounts.ContainsKey(id);
+    }
+
+    private static void CreateAccount(Dictionary<int, BankAccount> accounts, int id)
+    {
+        if (accounts.ContainsKey(id))
+        {
+            Console.WriteLine("Account already exists");
+        }
+        else
+        {
+            var acc = new BankAccount();
+            acc.Id = id;
+            accounts[id] = acc;
+        }
+    }
+}
+
