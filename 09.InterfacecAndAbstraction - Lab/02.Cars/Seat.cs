a691e1288312ee042c61d29d7b88ead3f03e6d7d
KrIsKa7a <kristiqn.ivanov@abv.bg>
2018-02-27 22:00:36 +0200

    Upload lab for interfaces and abstraction

---
+++ 09.InterfacecAndAbstraction - Lab/02.Cars/Seat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Seat : ICar
+{
+    public Seat(string model, string color)
+    {
+        this.Model = model;
+        this.Color = color;
+    }
+
+    public string Model
+    {
+        get; private set;
+    }
+
+    public string Color
+    {
+        get; private set;
+    }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendLine($"{this.Color} {GetType()} {this.Model}")
+            .AppendLine(Start())
+            .AppendLine(Stop());
+
+        var result = sb.ToString().TrimEnd();
+
+        return result;
+    }
+}
---
+++ 09.InterfacecAndAbstraction - Lab/02.Cars/Tesla.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class Tesla : ICar, IElectricCar
+{
+    public Tesla(string model, string color, int battery)
+    {
+        this.Model = model;
+        this.Color = color;
+        this.Battery = battery;
+    }
+
+    public string Model { get; private set; }
+
+    public string Color { get; private set; }
+
+    public int Battery { get; private set; }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+
+        sb.AppendLine($"{this.Color} {GetType()} {this.Model} with {this.Battery} Batteries")
+            .AppendLine(Start())
+            .AppendLine(Stop());
+
+        var result = sb.ToString().TrimEnd();
+
+        return result;
+    }
+}
